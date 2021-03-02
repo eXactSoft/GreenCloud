a1358d2e20cb7b05897d72ce12d1036bf676586f
eXactSoft <amrbox@yahoo.com>
2021-03-02 21:55:16 +0200

    Starting of Using vegas background jquery plugin

--- js/global.js
+++ js/global.js
@@ -2,8 +2,66 @@
 	Author URL: http://webthemez.com
 */
 $( function() {
-// Add background image
-	$.backstretch('images/datePalm1.jpg');
+	// Add background image
+	$('body').vegas({
+		delay: 20000,
+		transitionDuration: 3000,
+		slides: [
+			{ src: 'images/datePalm01.jpg' },
+			{ src: 'images/datePalm02.jpg' },
+			{ src: 'images/datePalm03.jpg' },
+			{ src: 'images/datePalm04.jpg' },
+			{ src: 'images/datePalm05.jpg' },
+			{ src: 'images/datePalm06.jpg' },
+			{ src: 'images/datePalm07.jpg' },
+			{ src: 'images/datePalm08.jpg' },
+			{ src: 'images/datePalm09.jpg' },	
+			{ src: 'images/datePalm10.jpg' },
+			{ src: 'images/datePalm11.jpg' },
+			{ src: 'images/datePalm12.jpg' },
+			{ src: 'images/datePalm13.jpg' },
+			{ src: 'images/datePalm14.jpg' },
+			{ src: 'images/datePalm15.jpg' },
+			{ src: 'images/datePalm16.jpg' },
+			{ src: 'images/datePalm17.jpg' },
+			{ src: 'images/datePalm18.jpg' },
+			{ src: 'images/datePalm19.jpg' },
+			{ src: 'images/datePalm20.jpg' },
+			{ src: 'images/datePalm21.jpg' },
+			{ src: 'images/datePalm22.jpg' },
+			{ src: 'images/datePalm23.jpg' },
+			{ src: 'images/datePalm24.jpg' },
+			{ src: 'images/datePalm25.jpg' },
+			{ src: 'images/datePalm26.jpg' },
+			{ src: 'images/datePalm27.jpg' },
+			{ src: 'images/datePalm28.jpg' },
+			{ src: 'images/datePalm29.jpg' },
+			{ src: 'images/datePalm30.jpg' },
+			{ src: 'images/datePalm31.jpg' },
+			{ src: 'images/datePalm32.jpg' },
+			{ src: 'images/datePalm33.jpg' },
+			{ src: 'images/datePalm34.jpg' },
+			{ src: 'images/datePalm35.jpg' },
+			{ src: 'images/datePalm36.jpg' },
+			{ src: 'images/datePalm37.jpg' },
+			{ src: 'images/datePalm38.jpg' },
+			{ src: 'images/datePalm39.jpg' },
+			{ src: 'images/datePalm40.jpg' },
+			{ src: 'images/datePalm41.jpg' },
+			{ src: 'images/datePalm42.jpg' },
+			{ src: 'images/datePalm43.jpg' },
+			{ src: 'images/datePalm44.jpg' },
+			{ src: 'images/datePalm45.jpg' }
+/*			
+				{ src: '../images/datePalm1.jpg' },
+				{ src: '../images/road2.jpg' },
+				{ src: '../images/datePalm.jpg' }
+*/				
+		],
+		//overlay: 'vegas/overlays/08.png',
+		animation: 'random'
+	});
+	
 	var endDate = "September 30, 2021 15:03:25";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
